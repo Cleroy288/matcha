14ad3d08bdb7842250376b26fe9e6d83969cf668
His000ka <pitroin@LGPierre.localdomain>
2026-03-21 15:53:52 +0100

    test pipeline: normalement  ok

--- backend/database/db.py
+++ backend/database/db.py
@@ -2,13 +2,13 @@ import psycopg2
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(override=False)
 
 def get_connection():
     return psycopg2.connect(
-        host="db",
-        database=os.getenv("POSTGRES_DB"),
-        user=os.getenv("POSTGRES_USER"),
-        password=os.getenv("POSTGRES_PASSWORD"),
-        port=5432
+        host=os.getenv("DB_HOST", "db"),
+        database=os.getenv("DB_NAME", os.getenv("POSTGRES_DB")),
+        user=os.getenv("DB_USER", os.getenv("POSTGRES_USER")),
+        password=os.getenv("DB_PASSWORD", os.getenv("POSTGRES_PASSWORD")),
+        port=int(os.getenv("DB_PORT", 5432))
     )
---
+++ backend/tests/test_auth.py
@@ -0,0 +1,43 @@
+import pytest
+import uuid
+from unittest.mock import patch
+from app import app
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+
+    with app.test_client() as client:
+        yield client
+
+@patch("services.auth_service.send_verification_email")  # neutralise l'envoi mail
+def test_register_login(client):
+    unique = uuid.uuid4().hex[:8]
+    email = f"test_{unique}@test.com"
+    username = f"user_{unique}"
+    
+    client.post("/register", json={
+        "username": username,
+        "email": email,
+        "password": "Password123*",
+        "first_name": "Test",
+        "last_name": "test"
+    })
+    assert response.status_code == 201
+    response = client.post("/login", json={
+        "username": username,
+        "password": "Password123*"
+    })
+    assert response.status_code == 400 #email not verified
+
+@patch("services.auth_service.send_reset_password_email")
+def test_reset_password(mock_mail, client):
+    response = client.post("/reset-password", json={
+        "email": "test@test.com"
+    })
+    assert response.status_code in [200, 404]
+
+def test_me_unauthorized(client):
+    response = client.get("/me")
+
+    assert response.status_code in [401, 403]
