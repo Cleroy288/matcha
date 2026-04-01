b54b8b73be536800508494e32af0b95ab942c930
His000ka <pitroin@LGPierre.localdomain>
2026-04-01 22:35:19 +0200

    adding svg, create style svg with animation

--- matcha-frontend/src/pages/Login.tsx
+++ matcha-frontend/src/pages/Login.tsx
@@ -7,6 +7,15 @@ import { Link } from "react-router-dom"
 import { API_ROUTES, fetchWithCredentials } from "../config/api.ts"
 import StatusMessage from "../components/StatusMessage.tsx"
 
+import TwoCoeur from "../assets/doublecoeur.svg?react"
+import Couple from "../assets/cup2.svg?react"
+import Cup4 from "../assets/cup4.svg?react"
+import Cupi from "../assets/coeurcupidon.svg?react"
+// import Symbole2 from "../assets/love.svg?react"
+import CupTchin from "../assets/cupTchin.svg?react"
+import OiseauVol from "../assets/oiseauCoeur.svg?react"
+
+
 export default function Login() {
   const { setUser } = useAuth()
   const [username, setUsername] = useState("")
@@ -37,6 +46,16 @@ export default function Login() {
 
   return (
     <div className="app-container">
+        <TwoCoeur className="sil sil-accent sil-left-in sil-xs sil-middle-up sil-flip-x sil-float" />
+        <Cup4 className="sil sil-primary sil-left-in sil-xs sil-middle-cup4 sil-float-alt" />
+        
+        
+        <OiseauVol className="sil sil-accent sil-left-mid-center sil-xs sil-middle-down sil-sway-hg sil-shadow-primary" />
+        
+        <CupTchin className="sil sil-color-topbar-border sil-left-mid sil-sm sil-bottom sil-flip-x" />
+        {/* <Symbole2 className="sil sil-primary sil-left sil-xs sil-bottom " /> */}
+        <Cupi className="sil sil-color-topbar-border sil-right-mid sil-xxs sil-xs sil-middle-cupi sil-sway-shadow sil-shadow-accent" />
+        <Couple className="sil sil-accent sil-right-in sil-md " />
         <Topbar></Topbar>
       <h1>Se connecter</h1>
       {error && <StatusMessage type="error" message={error} onClose={() => setError(null)}/>} 
